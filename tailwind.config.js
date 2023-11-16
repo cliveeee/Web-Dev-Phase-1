420328f08eae50d54fa52ae7e288c54b4c472214
cliveeee <20095317@tafe.wa.edu.au>
2023-11-17 07:30:12 +0800

    feat(phase-50:TailwindCSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './index.html',
+    './phase-*/*.html',
+    './pages/**/*.html',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
